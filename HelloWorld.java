e2223b05b2e7d477d3448d7cc2f6b6668b65166b
JasmineBaclig <jasminebaclig@yahoo.com>
2020-08-20 16:49:05 +0000

    Completed the assignment

---
+++ HelloWorld.java
@@ -0,0 +1,47 @@
+//Jasmine Baclig
+//AP Computer Science A - Period 3
+//August 20, 2020
+
+public class HelloWorld {
+  public static void main(String[] args) {
+    /*I created a guitar since I do play it for fun*/
+    
+    System.out.println("░░░░█████");
+    System.out.println("░░░█░░████");
+    System.out.println("░░░██░██████");
+    System.out.println("░░░█████████");
+    System.out.println("░░█████████");
+    System.out.println("░░██████░░░█");
+    System.out.println("░░░██████░░░█");
+    System.out.println("░░░░██████░░░█");
+    System.out.println("░░░░░██░░██░░░██");
+    System.out.println("░░░░░░░░░░██░░░██");
+    System.out.println("░░░░░░░░░░░██░░░██░░██████");
+    System.out.println("░░░░░░░░░░░░██░░░█████░░░██");
+    System.out.println("░░░░░░░░░░░░░██░░░██░░░░░░██");
+    System.out.println("░░░░░░░░░░░░░░██░░░██░░░░░░██");
+    System.out.println("░░░░░░░░░░░░░░░██░░░██░░░░░░██");
+    System.out.println("░░░░░░░░░░░░░░░███░░░░█░░░░░██");
+    System.out.println("░░░░░░░░░░░░░░██░██░░░░█░░░░░████");
+    System.out.println("░░░░░░░░░░░░░░██░░██░░░░█░░░░░░████");
+    System.out.println("░░░░░░░░░░░░░░█░░░░██░░░█░░░░░░░░░██");
+    System.out.println("░░░░░░░░░░░░░░██░░░░██░█████░░░░░░░██");
+    System.out.println("░░░░░░░░░░░░░░██░░░░░██░█████░░░░░░░██");
+    System.out.println("░░░░░░░░░░░░░░███░░░░░░░█████░░░░░░░░██");
+    System.out.println("░░░░░░░░░░░░░░█░██░░░░░░████░░░░░░░░░██");
+    System.out.println("░░░░░░░░░░░░░░██░░███░░░░░░░░░░░░░░░░██");
+    System.out.println("░░░░░░░░░░░░░░░██░░░██░░░░░░░░░░░░░░░██");
+    System.out.println("░░░░░░░░░░░░░░░░░██░░██░░░░░░░░░░░░░░██");
+    System.out.println("░░░░░░░░░░░░░░░░░░██░░█░░░░░░░░░░░░░░██");
+    System.out.println("░░░░░░░░░░░░░░░░░░░██░█░░░░░░░░░░░░░███");
+    System.out.println("░░░░░░░░░░░░░░░░░░░░█░█░░░░░░░░░░░░██░█");
+    System.out.println("░░░░░░░░░░░░░░░░░░░░██░█░░░░░░░░░░██░█");
+    System.out.println("░░░░░░░░░░░░░░░░░░░░██░██░░░░░░░░██░░█");
+    System.out.println("░░░░░░░░░░░░░░░░░░░░░█░░██░░░░░███░░█");
+    System.out.println("░░░░░░░░░░░░░░░░░░░░░█░░░███████░░░██");
+    System.out.println("░░░░░░░░░░░░░░░░░░░░░░█░░░░███░░░░██");
+    System.out.println("░░░░░░░░░░░░░░░░░░░░░░██░░░░░░░░██");
+    System.out.println("░░░░░░░░░░░░░░░░░░░░░░░██████████");
+    System.out.println("░░░░░░░░░░░░░░░░░░░░░░░░░██████");
+  }
+}
